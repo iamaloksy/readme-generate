df5c664f3956e116559ad4d45d260aa802600146
Alok Kumar <137317651+iamaloksy@users.noreply.github.com>
2024-01-19 09:28:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+<h2 align="center">Readme-Generate </h2>
+
+![Generate Readme](images/banner.png)
+<h4 align="center"> Generate Readme </h4>
+
+<p align="center">
+<br>
+    <img src="https://img.shields.io/badge/Author-iamalok-magenta?style=flat-square">
+    <img src="https://img.shields.io/badge/Open%20Source-Yes-orange?style=flat-square">
+    <img src="https://img.shields.io/badge/Maintained-Yes-cyan?style=flat-square">
+    <img src="https://img.shields.io/badge/Written%20In-JavaScript-blue?style=flat-square">
+</p>
+
+### [+] Description
+readme generator is a tool that allows you to create simple and beautiful readme for your repository that you can copy/paste.
+
+### [+] Instructions
+
+- Go to: <a href="https://iamaloksy.github.io/readme-generate/">Readme-Generate</a>
+- Fill any inputs you like
+- Copy the code generated in the Code section
+- Paste the copied code into the readme file of the repository and save
+- Enjoy!
+
+### [+] Preview
+
+![Readme-Generate](images/preview.gif)
+
+### [+] Credits 
+<a href="https://github.com/arturssmirnovs/github-profile-readme-generator">Arturssmirnovs</a>
+
+### [+] Find me on 
+ <a href="mailto:kr.alok.sy@gmail.com" target="_blank"><img src="https://img.shields.io/badge/Email-kr.alok.sy@gmail.com-blue?style=for-the-badge&logo=gmail"></a>
+
+<a href="https://m.me/iamaloksy" target="_blank"><img src="https://img.shields.io/badge/Messenger-iamaloksy-blue?style=for-the-badge&logo=messenger"></a>
+
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+/*
+  Source Code Owner: https://github.com/arturssmirnovs
+  Current Code Owner: https://github.com/iamaloksy 
+  
+  Repository readme generator is a tool that allows you to create simple and beautiful readme for your repository that you can copy or paste or download. Made with Vue.js.
+  
+  Author: KasRoudra
+  Messenger: https://www.linkedin/in/iamaloksy
+  Email: kr.alok.sy@gmail.com
+  Date: 18-01-2024
+  Site: https://iamaloksy.github.io/readme-generate
+  Main Language: Javascript
+*/
+
+/*
+MIT License
+
+Copyright (c) 2024 Alok
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+?>
+<script type="text/javascript">
+document.location = "index.html";
+</script> 
